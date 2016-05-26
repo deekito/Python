7e1ecc14b8730484efe3fcf6abf345d78ef1374b
deekito <deekito@126.com>
2016-05-26 16:07:19 +0800

    Guess Number Game

--- com/NumberGuess.py
+++ com/NumberGuess.py
@@ -2,26 +2,27 @@
 # -*-coding:utf-8-*-
 import random
 
-class NumberGuess():
 
+class NumberGuess():
     flag = True
 
     def __init__(self, number):
         self.__number = number
 
-    def getNumber(self):
+    def getnumber(self):
         return self.__number
+
 
 N = NumberGuess(random.randint(0, 100))
 
 print N.getNumber()
 
-while(NumberGuess.flag):
+while (NumberGuess.flag):
     userGuess = int(raw_input('请输入一个0～100的数字:'))
-    if userGuess > int(N.getNumber()):
+    if userGuess > int(N.getnumber()):
         print '你猜的数字太大了'
         continue
-    elif userGuess < int(N.getNumber()):
+    elif userGuess < int(N.getnumber()):
         print '你猜的数字太小了'
         continue
     else:
